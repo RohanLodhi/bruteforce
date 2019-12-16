ad7cc78d39380c4242a846ef24a5598c9514f8d6
RohanLodhi <rohanlodhi619@gmail.com>
2019-12-16 18:36:53 +0530

    add existing files

--- bruteforce.py
+++ bruteforce.py
@@ -1,12 +1,25 @@
-from zipfile import ZipFile
-passwordList = []
-a = open("dictionary.txt", "r")
-for line in a.readlines():
-    password = line.strip()
-    passwordList.append(password)
+import zipfile
 
-for entry in passwordList:
-    with ZipFile('test.zip') as zf:
+
+count = 1
+
+with open('dictionary.txt','rb') as text:
+    for entry in text.readlines():
+        password = entry.strip()
         try:
-            zf.extractall(pwd=entry)
-            print '\n***** Password: {} *****\n'.format(entry)
+            with zipfile.ZipFile('bin.zip','r') as zf:
+                zf.extractall(pwd=password)
+
+                data = zf.namelist()[0]
+
+                data_size = zf.getinfo(data).file_size
+
+                print('''******************************\n[+] Password found! ~ %s\n ~%s\n ~%s\n******************************''' 
+                    % (password.decode('utf8'), data, data_size))
+                break
+
+        except:
+            number = count
+            print('[%s] [-] Password failed! - %s' % (number,password.decode('utf8')))
+            count += 1
+            pass
